37e1e53ee880046c74d11fe2e2bfaeeffd8bd45c
leon820221 <88069641+leon820221@users.noreply.github.com>
2022-12-28 04:25:59 +0000

    v1

--- index.js
+++ index.js
@@ -2,8 +2,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const stuList = [
+  {
+    id:'001',
+    name: 'Jack',
+    age: 22,
+    gender: 'Male',
+    hobby: 'Boxing'
+  },
+  {
+    id:'002',
+    name: 'Lucy',
+    age: 20,
+    gender: 'Female',
+    hobby: 'Drawing'
+  }
+]
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+  next()
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(stuList)
 })
 
 app.listen(port, () => {
